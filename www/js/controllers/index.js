1e779a139f2f0b01a21d7b3cd2ee2b1fdf45bfd8
Mikhail Bolsunovskiy <bolsunovskiy.mikhail@techs.com.ua>
2014-12-16 10:12:48 +0200

    some fixes

--- www/js/controllers/index.js
+++ www/js/controllers/index.js
@@ -9,70 +9,128 @@ define([
     //var connectedDevices = [];
     var devices = [];
     var view;
+    var loopCounter = 0;
 
     var loop = function(){
 
-        //for(var i=0;i<connectedDevices.length;i++){
-        //    bluetoothle.write(function (result) {
-        //        console.log(JSON.stringify(result));
-        //    }, function (error) {
-        //        console.log(JSON.stringify(error));
-        //        for(var j=0;j<connectedDevices.length;j++) {
-        //            if(connectedDevices[j].address == error.address) {
-        //                connectedDevices.splice(j, 1);
-        //                break;
-        //            }
-        //        }
-        //
-        //        for(var j=0;j<devices.length;j++) {
-        //            if(devices[j].address == error.address) {
-        //                devices[j].connected = false;
-        //                break;
-        //            }
-        //        }
-        //
-        //        reconnect(error.address);
-        //
-        //    }, {
-        //        "value": btoa("AT"),
-        //        "serviceUuid": "ffe0",
-        //        "characteristicUuid": "ffe1",
-        //        "type": "noResponse",
-        //        "address": connectedDevices[i].address
-        //    });
-        //}
 
-        //try to connect for not connected devices
-        //for(var j=0;j<devices.length;j++){
-        //    if(typeof(devices[j].connected) == 'undefined' /*|| devices[j].connected == false*/) {
-        //        //connect(devices[j].address);
-        //        reconnect(devices[j].address);
-        //    }
-        //}
+        for(var i=0;i<devices.length;i++) {
+            if(typeof(devices[i].connected) != 'undefined' && devices[i].connected == true) {
+
+                var value = "AT";
+                if(loopCounter % 5 == 0)
+                    value = "AT+BATT?";
+
+                bluetoothle.write(function (result) {
+                    console.log(JSON.stringify(result));
+                    console.log(loopCounter);
+                }, function (error) {
+                    console.log(JSON.stringify(error));
+
+                    setConnected(error.address, false);
+
+                    bluetoothle.disconnect(function(result){
+                        console.log(JSON.stringify(result));
+                        if(result.status == "disconnected") {
+                            bluetoothle.close(function(result){
+                                console.log(JSON.stringify(result));
+                                //connect(result.address);
+                            }, function(result){
+                                console.log(JSON.stringify(result));
+                            }, {address: error.address});
+                        }
+                    }, function(result){
+                        console.log(JSON.stringify(result));
+                    }, {address: error.address});
+
+                }, {
+                    "value": btoa("AT"),
+                    "serviceUuid": "ffe0",
+                    "characteristicUuid": "ffe1",
+                    "type": "noResponse",
+                    "address": devices[i].address
+                });
+            }
+        }
+
+        loopCounter++;
 
 
-        //console.log(JSON.stringify(devices));
-        //console.log(JSON.stringify(connectedDevices));
 
         setTimeout(loop, 5000);
     };
 
-    var reconnect = function(_address){
-        console.log("reconnecting...");
-        view.setOffline(_address);
+    var setConnected = function(_address, value){
+        for(var j=0;j<devices.length;j++) {
+            if(devices[j].address == _address) {
+                devices[j].connected = value;
+                break;
+            }
+        }
+    };
+
+    var disconnect = function(_address, callback){
         bluetoothle.disconnect(function(result){
-            JSON.stringify(result);
-            bluetoothle.close(function(result){
-                JSON.stringify(result);
-                connect(result.address);
-            }, function(result){
-                JSON.stringify(result);
-            }, {address: result.address});
+            console.log(JSON.stringify(result));
+            if(result.status == "disconnected") {
+                bluetoothle.close(function(result){
+                    console.log(JSON.stringify(result));
+                    callback(result.address);
+                }, function(result){
+                    console.log(JSON.stringify(result));
+                }, {address: result.address});
+            }
         }, function(result){
-            JSON.stringify(result);
-        }, {
-            address: _address
-        });
+            console.log(JSON.stringify(result));
+        }, {address: _address});
+    };
+
+    var reconnect = function(_address){
+        view.setOffline(_address);
+        setConnected(_address, false);
+
+        bluetoothle.isConnected(function (result) {
+            console.log(JSON.stringify(result));
+
+            if (result.error == "neverConnected" ) {
+                console.log("neverConnected");
+                connect(result.address);
+            } else if(result.isConnected == true) {
+                console.log("isConnected == true!");
+
+                setConnected(result.address, true);
+                view.setOnline(result.address);
+            }
+            else if(result.isConnected == false) {
+                console.log("isConnected == false!");
+
+                /*bluetoothle.close(function(result){
+                    JSON.stringify(result);
+                    connect(result.address);
+                }, function(result){
+                    JSON.stringify(result);
+                }, {address: result.address});*/
+
+                bluetoothle.disconnect(function(result) {
+                    bluetoothle.reconnect(function (result) {
+                        console.log(JSON.stringify(result));
+                        setConnected(result.address, true);
+                        view.setOnline(result.address);
+                    }, function (result) {
+                        console.log(JSON.stringify(result));
+                    }, {
+                        address: result.address
+                    });
+                }, function(result){
+                    console.log("DISCONNECT ERROR!");
+                    console.log(JSON.stringify(result));
+                }, {
+                    address: result.address
+                });
+
+            }
+        }, {address:_address});
+
     };
 
     var connect = function(_address) {
@@ -83,43 +141,15 @@ define([
                         bluetoothle.subscribe(function (result) {
 
                             if (result.status == "subscribed") {
-                                for (var j = 0; j < devices.length; j++) {
-                                    if (devices[j].address == result.address) {
-                                        devices[j].connected = true;
-                                    }
-                                }
 
+                                setConnected(result.address, true);
                                 view.setOnline(result.address);
                             }
 
-                            /*if (result.status == "subscribedResult") {
-                                var value = atob(result.value);
-                                var device = Database.getByAddress(result.address);
-                                if (device != false) {
-                                    if (value.indexOf(device.name) != -1) {
-                                        connectedDevices.push(device);
-                                        view.setOnline(device.address);
-                                        for (var j = 0; j < devices.length; j++) {
-                                            if (devices[j].address == result.address) {
-                                                devices[j].connected = true;
-                                            }
-                                        }
-                                    }
-                                }
-                            } else if (result.status == "subscribed") {
+                            //TODO: Place logic here
 
-                                bluetoothle.write(function (result) {
-                                    console.log(JSON.stringify(result));
-                                }, function (error) {
-                                    console.log(JSON.stringify(error));
-                                }, {
-                                    "value": btoa("AT+NAME?"),
-                                    "serviceUuid": "ffe0",
-                                    "characteristicUuid": "ffe1",
-                                    "type": "noResponse",
-                                    "address": result.address
-                                });
-                            }*/
+
+
                         }, function (error) {
                             console.log(JSON.stringify(error));
                         }, {
@@ -136,10 +166,13 @@ define([
             }
         }, function (error) {
             console.log(JSON.stringify(error));
+            setConnected(error.address, false);
+            view.setOffline(error.address);
             //TODO: fix reconnect on connection error!!!
-            reconnect(error.address);
-            //connect(error.address);
-            //connect(error.address);
+
+            disconnect(error.address, function(_address){
+                connect(_address);
+            });
         }, {address: _address});
     };
 
@@ -150,72 +183,12 @@ define([
             devices: devices
         });
 
-        //connectedDevices = [];
 
 
         bluetoothle.initialize(function () {
 
             for (var i = 0; i < devices.length; i++) {
-                bluetoothle.isConnected(function (result) {
-                    //console.log(JSON.stringify(result));
-                    if (result.error == "neverConnected") {
-                        connect(result.address);
-                        /*bluetoothle.connect(function (result) {
-                            if (result.status == "connected") {
-                                bluetoothle.discover(function (result) {
-                                    if (result.status == "discovered") {
-                                        bluetoothle.subscribe(function (result) {
-                                            if (result.status == "subscribedResult") {
-                                                var value = atob(result.value);
-                                                var device = Database.getByAddress(result.address);
-                                                if(device != false) {
-                                                    if (value.indexOf(device.name) != -1) {
-                                                        connectedDevices.push(device);
-                                                        view.setOnline(device);
-                                                        for(var j=0;j<devices.length;j++) {
-                                                            if(devices[j].address == result.address) {
-                                                                devices[j].connected = true;
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            } else if (result.status == "subscribed") {
-
-                                                bluetoothle.write(function (result) {
-                                                    console.log(JSON.stringify(result));
-                                                }, function (error) {
-                                                    console.log(JSON.stringify(error));
-                                                }, {
-                                                    "value": btoa("AT+NAME?"),
-                                                    "serviceUuid": "ffe0",
-                                                    "characteristicUuid": "ffe1",
-                                                    "type": "noResponse",
-                                                    "address": result.address
-                                                });
-                                            }
-                                        }, null, {
-                                            address: result.address,
-                                            "serviceUuid": "ffe0",
-                                            "characteristicUuid": "ffe1",
-                                            "isNotification": true
-                                        });
-                                    }
-                                }, function () {
-                                }, {address: result.address});
-                            }
-                        }, function () {
-                        }, {address: result.address});*/
-                    } else if(result.isConnected) {
-                        var device = Database.getByAddress(result.address);
-                        //connectedDevices.push(device);
-                        view.setOnline(device.address);
-                        for(var j=0;j<devices.length;j++) {
-                            if(devices[j].address == result.address) {
-                                devices[j].connected = true;
-                            }
-                        }
-                    }
-                }, {address: devices[i].address});
+                reconnect(devices[i].address);
             }
 
             view.render();
@@ -229,7 +202,7 @@ define([
 
     };
 
-    loop();
+    //loop();
 
     return {
         init: init
--- www/js/views/addDevice.js
+++ www/js/views/addDevice.js
@@ -9,17 +9,38 @@ define([
     'text!html/error.html',
     'models/database',
     'text!html/error.html',
-    'text!html/info.html'
-], function($, _, Backbone, Router, Device, _index, _item, _error, Database, _error, _info){
+    'text!html/info.html',
+    'text!html/addDevice/connecting.html',
+], function($, _, Backbone, Router, Device, _index, _item, _error, Database, _error, _info, _connecting){
+
+    var devices;
 
     var AddDeviceView = Backbone.View.extend({
         initialize: function(){
-
+            devices = [];
         },
         events: {
             "click #do-scan": "startScan",
             "click #stop-scan": "stopScan",
-            "click .item": "selectDevice"
+            "click .item": "selectDevice",
+            "click .cancel": "cancelConnection"
+        },
+
+        cancelConnection: function(e){
+            var address = $(e.target).data("address");
+            $(e.target).parent().hide();
+            bluetoothle.disconnect(function(result){
+                bluetoothle.close(null, null, {address: result.address});
+            }, null, {address: address});
+        },
+
+        setConnected: function(_address, name) {
+            $.each($("#errors").find("button"), function(i, o) {
+                if($(o).data("address") == _address) {
+                    $(o).parent().removeClass("btn-info").addClass("btn-success");
+                    $(o).parent().html("Successfully connected to &laquo;" + name + "&raquo;");
+                }
+            });
         },
 
         selectDevice: function(e){
@@ -30,7 +51,10 @@ define([
             var name = $(e.target).html();
             var error = _.template(_error);
             var info = _.template(_info);
-            $("#errors").html(info({info: "Connecting to " + name + "..."}));
+            var connecting = _.template(_connecting);
+            var thisView = this;
+
+            $("#errors").append(info({info: connecting({name: name, address: address})}));
 
             bluetoothle.connect(function(result){
                 if(result.status == "connected") {
@@ -38,10 +62,19 @@ define([
                         if(result.status == "discovered") {
                             bluetoothle.subscribe(function(result){
                                 if(result.status == "subscribedResult") {
+                                    //console.log(JSON.stringify(result));
                                     var value = atob(result.value);
-                                    if(value.indexOf(name) != -1) {
+                                    //console.log(value);
+                                    //if(value.indexOf(name) != -1) {
+                                    //    Database.addDevice(name, address);
+                                    //    Router.navigate("index", {trigger: true});
+                                    //}
+                                    if(value.indexOf("+NAME") != -1) {
+                                        var name = value.substr(value.indexOf(":") + 1);
+                                        //console.log(name.trim());
                                         Database.addDevice(name, address);
-                                        Router.navigate("index", {trigger: true});
+                                        thisView.setConnected(address, name);
+                                        //Router.navigate("index", {trigger: true});
                                     }
                                 } else if(result.status == "subscribed") {
 
@@ -83,6 +116,7 @@ define([
         },
 
         startScan: function(){
+            devices = [];
             bluetoothle.stopScan();
             $("#do-scan").hide();
             $("#stop-scan").show();
@@ -93,7 +127,24 @@ define([
 
             bluetoothle.startScan(function(result){
                 if(result.status == "scanResult") {
-                    $("#devices").append(item({name : result.name, address: result.address}));
+
+                    var toAdd = true;
+
+                    for(var i=0;i<devices.length;i++) {
+                        if(devices[i].address == result.address) {
+                            toAdd = false;
+                            break;
+                        }
+                    }
+
+                    if(toAdd) {
+                        $("#devices").append(item({name: result.name, address: result.address}));
+                        devices.push({
+                            address: result.address,
+                            name: result.name
+                        });
+                    }
+
                 }
             }, function() {
                 $el.html(tplError({error: "Bluetooth initialize error. Please enable bluetooth and restart the app."}))
